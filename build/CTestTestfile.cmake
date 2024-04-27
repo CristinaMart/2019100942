ba1e680582b477375dd0c7ea40d2c69ba6aec328
unknown <DELL@DESKTOP-31TLK3V.unida.edu.py>
2024-04-27 10:53:08 -0400

    Agregado código de movimiento de tortuga y configuraciones de ROS

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Windows/System32/Cristina_Martinez/src
+# Build directory: C:/Windows/System32/Cristina_Martinez/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("turtle_unida")
---
+++ build/catkin_generated/setup_cached.bat
@@ -0,0 +1,11 @@
+@echo off
+REM generated from catkin/python/catkin/environment_cache.py
+
+REM based on a snapshot of the environment before and after calling the setup script
+REM it emulates the modifications of the setup script without recurring computations
+
+REM new environment variables
+
+REM modified environment variables
+set CMAKE_PREFIX_PATH=C:\opt\ros\melodic\x64\tools\vcpkg\installed\x64-windows;C:/Windows/System32/Cristina_Martinez/devel;C:/opt/ros/melodic/x64
+set ROS_PACKAGE_PATH=C:/Windows/System32/Cristina_Martinez/src;%ROS_PACKAGE_PATH%
